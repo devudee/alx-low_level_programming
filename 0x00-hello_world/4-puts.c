9ffc5db887b4c4186e2dc33433a49543ad1ca28e
devudee <u.uduakdavid@gmail.com>
2023-04-20 08:41:40 -0700

    welcome Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
- * main - print a line of code using the put function
+/**
+ * main -print a line of code using the put function
  *
  * Return: 0
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
-
+}
