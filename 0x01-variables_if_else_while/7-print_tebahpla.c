1f6c268dd0af0050f7d2191d7ef3360bc1989cd8
devudee <u.uduakdavid@gmail.com>
2023-04-21 15:14:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part
+ * Return: 0
+ */
+int main(void)
+{
+	char ud;
+
+	for (ud = 'z'; ud >= 'a'; ud--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
