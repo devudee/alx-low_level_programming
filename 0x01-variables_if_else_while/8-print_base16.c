c438f52392d02bfd7cafd50da0a9ea9f436805cd
devudee <u.uduakdavid@gmail.com>
2023-04-21 15:29:57 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.n>
+
+/**
+ * main - printing all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char ud;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ud = 'a'; ud <= 'f'; ud++)
+	{
+		putchar(ud);
+	}
+	putchar('\n');
+	return (0);
+}
